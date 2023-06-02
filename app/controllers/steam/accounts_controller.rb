e5303f7a304ba1464b3c8fbbaa34f5d5b928946e
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-06-02 15:15:45 -0300

    Add ability

--- app/controllers/steam/accounts_controller.rb
+++ app/controllers/steam/accounts_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Steam::AccountsController < ApplicationController
+  load_and_authorize_resource
+
   def index
-    @steam_accounts = Steam::Account.all
+    @steam_accounts = Steam::Account.accessible_by(current_ability)
   end
 end
