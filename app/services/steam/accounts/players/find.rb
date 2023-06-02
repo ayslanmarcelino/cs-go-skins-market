cc9a8e54dea3acf730c3acbcf6d4d42524b95e77
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-06-02 18:14:55 -0300

    Add Players Steam find service

---
+++ app/services/steam/accounts/players/find.rb
@@ -0,0 +1,36 @@
+module Steam
+  module Accounts
+    module Players
+      class Find < ApplicationService
+        def initialize(steam_custom_id:)
+          @steam_custom_id = steam_custom_id
+        end
+
+        def call
+          find
+        end
+
+        private
+
+        def find
+          JSON.parse(response.body)['response']['players'].first
+        end
+
+        def response
+          @response ||= RestClient.get(endpoint)
+        end
+
+        def steam_id
+          @steam_id ||= Steam::Accounts::SteamIds::Find.call(steam_custom_id: @steam_custom_id)
+        end
+
+        def endpoint
+          @endpoint ||= "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=#{ENV['STEAM_API_KEY']}&steamids=#{steam_id}"
+        end
+      end
+    end
+  end
+end
+
+
+
