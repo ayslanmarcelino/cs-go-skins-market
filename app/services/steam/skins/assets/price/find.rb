a348d6159516130e2c45e160a4e092b0ae14ae72
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-06-03 23:54:28 -0300

    Add find skin price service

---
+++ app/services/steam/skins/assets/price/find.rb
@@ -0,0 +1,43 @@
+module Steam
+  module Skins
+    module Assets
+      module Price
+        class Find < ApplicationService
+          def initialize(skin_name:)
+            @skin_name = skin_name
+          end
+
+          def call
+            find
+          end
+
+          private
+
+          def find
+            price.nil? ? 0 : price.gsub(/[^\d,]/, '').sub(',', '.').to_f
+          end
+
+          def price
+            @price ||= parsed_response['lowest_price']
+          end
+
+          def parsed_response
+            @parsed_response ||= JSON.parse(response.body)
+          end
+
+          def response
+            @response ||= RestClient.get(endpoint)
+          end
+
+          def endpoint
+            "https://steamcommunity.com/market/priceoverview/?currency=7&appid=730&market_hash_name=#{escaped_name}"
+          end
+
+          def escaped_name
+            CGI.escape(@skin_name)
+          end
+        end
+      end
+    end
+  end
+end
