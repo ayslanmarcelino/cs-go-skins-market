e2d532dfdac8faab55f71297ba59b104d1033c72
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-06-04 01:40:02 -0300

    Add initial index

--- app/controllers/skins_controller.rb
+++ app/controllers/skins_controller.rb
@@ -1,12 +1,20 @@
 class SkinsController < ApplicationController
+  load_and_authorize_resource
+
   before_action :steam_account, only: :search
 
   def index
-    @skins = Skin.all
+    @query = Skin.includes(:steam_account)
+                 .order(created_at: :desc)
+                 .accessible_by(current_ability)
+                 .page(params[:page])
+                 .ransack(params[:q])
+
+    @skins = @query.result(distinct: false)
   end
 
   def search
-    @skins ||= Steam::Skins::Create.call(steam_account: steam_account)
+    @search ||= Steam::Skins::Create.call(steam_account: steam_account)
   end
 
   private
