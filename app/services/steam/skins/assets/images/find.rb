e68cc51d5b35b001bfc928d54927c28748324a61
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-06-03 22:07:24 -0300

    Add find skin image service

---
+++ app/services/steam/skins/assets/images/find.rb
@@ -0,0 +1,27 @@
+module Steam
+  module Skins
+    module Assets
+      module Images
+        class Find < ApplicationService
+          def initialize(skin:)
+            @skin = skin
+          end
+
+          def call
+            find
+          end
+
+          private
+
+          def find
+            "https://steamcommunity-a.akamaihd.net/economy/image/#{image}"
+          end
+
+          def image
+            @skin['icon_url_large'].present? ? @skin['icon_url_large'] : @skin['icon_url']
+          end
+        end
+      end
+    end
+  end
+end
