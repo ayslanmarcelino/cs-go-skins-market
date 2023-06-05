deaa340c2b0aa4ff83c85d69c5bce7dc6d9d92a1
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-06-05 20:39:11 -0300

    Add updated_at to transactions

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,6 +4,7 @@
 #
 #  id                  :bigint           not null, primary key
 #  aasm_state          :string           default("pending")
+#  identifier          :integer
 #  value               :float
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
