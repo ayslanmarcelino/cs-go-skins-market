f5bca0bac43698c7bf84bb201b3541340a953829
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-06-10 01:54:47 -0300

    Add profit chart by month to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
+  before_action :finished_transactions_by_month, only: :index
+
   def index; end
 
   def pdf
@@ -7,5 +9,30 @@ class DashboardController < ApplicationController
       layout: 'application',
       template: 'dashboard/report'
     )
+  end
+
+  private
+
+  def finished_transactions_by_month
+    @finished_transactions_by_month ||= current_user.transactions
+                                                    .finisheds
+                                                    .joins(:transaction_type)
+                                                    .group(month_trunc_sql)
+                                                    .select(
+                                                      month_trunc_sql.as("month"),
+                                                      Arel.sql(
+                                                        "SUM(transactions.value - COALESCE(skins.amount_paid, 0)) AS profit"
+                                                      )
+                                                    )
+                                                    .left_joins(:skins)
+                                                    .order(month_trunc_sql)
+                                                    .pluck(
+                                                      month_trunc_sql,
+                                                      Arel.sql("SUM(transactions.value - COALESCE(skins.amount_paid, 0))")
+                                                    )
+  end
+
+  def month_trunc_sql
+    Arel.sql("DATE_TRUNC('month', transactions.created_at)::date")
   end
 end
