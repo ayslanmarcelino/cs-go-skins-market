e6fee0cee7ee66ae8097892ebd9887714f34d32c
ayslanmarcelino <ayslanmarcelino@gmail.com>
2024-03-04 00:44:58 -0300

    feat: add max retries and interval time to retry

--- app/jobs/inventories/skins/create_job.rb
+++ app/jobs/inventories/skins/create_job.rb
@@ -1,5 +1,7 @@
 class Inventories::Skins::CreateJob < ApplicationJob
   queue_as :default
+  sidekiq_options retry: 3
+  sidekiq_retry_in { 360 }
 
   def perform(current_user:, steam_account:)
     Inventories::Skins::Create.call(current_user: current_user, steam_account: steam_account)
