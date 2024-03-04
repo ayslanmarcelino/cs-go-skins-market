f13ce19550a9773da64ee1cb77693e2018d461ea
ayslanmarcelino <ayslanmarcelino@gmail.com>
2024-03-04 00:57:28 -0300

    refactor: inverval time to retry job

--- app/jobs/inventories/skins/create_job.rb
+++ app/jobs/inventories/skins/create_job.rb
@@ -1,7 +1,7 @@
 class Inventories::Skins::CreateJob < ApplicationJob
   queue_as :default
   sidekiq_options retry: 3
-  sidekiq_retry_in { 360 }
+  sidekiq_retry_in { 900 }
 
   def perform(current_user:, steam_account:)
     Inventories::Skins::Create.call(current_user: current_user, steam_account: steam_account)
